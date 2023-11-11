63692fdbfb5ff30f5dda36eac22112aeedf6b65d
Aarush Pal <aarushpal2@gmail.com>
2023-11-11 13:01:49 +0530

    updated experiences

--- components/experience.tsx
+++ components/experience.tsx
@@ -44,7 +44,9 @@ export default function Experience() {
                 fontSize: "1.5rem",
               }}
             >
-              <h3 className="font-semibold capitalize">{item.title}</h3>
+              <h3 className="font-semibold capitalize">
+                {item.title} - <span className="font-bold">{item.company}</span>{" "}
+              </h3>
               <p className="font-normal !mt-0">{item.location}</p>
               <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
                 {item.description}
